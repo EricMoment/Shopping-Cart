1f63619bdafc5011b56212797087a1961f0385ab
Eric <a365900630@gmail.com>
2024-01-27 11:20:49 +0900

    changed header link position

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,12 @@ import './Header.css'
 const Header = ({count}) => {
   return (
     <div className="header">
-      <div className='headerHome'>
-      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="green" viewBox="0 0 16 16">
-        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-      </svg>        
-        <a href='/' >VanillaGoods.com</a>
-      </div>
+      <a href='/' className='headerHome'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="green" viewBox="0 0 16 16">
+          <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+        </svg>        
+        <div>VanillaGoods.com</div>
+      </a>
       <div className="headerNavi">
         <a href='/cart'>
           <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="green" viewBox="0 0 16 16">
